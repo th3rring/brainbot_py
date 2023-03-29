8b2cd17ea5c58ff468a6a67dd267b39b03a4f853
Thomas Herring <teh6@rice.edu>
2023-03-29 16:07:26 -0500

    Added manifold vis

--- start_jupyter.sh
+++ start_jupyter.sh
@@ -1,7 +1,13 @@
 #! /bin/bash
+# Author: Thomas Herring
+# License: MIT
+
+"""
+This script starts a jupyter notebook server in a tmux session. Useful for running persistent but managed processes.
+"""
 
 # tmux new-session -d -s "jupyter" jupyter lab --ip=0.0.0.0 --port=8888 --allow-root --no-browser --NotebookApp.token='robobio'
 # jupyter lab --ip=0.0.0.0 --port=8888 --allow-root --no-browser --NotebookApp.token='robobio'
 # jupyter notebook --ip=0.0.0.0 --port=8888 --allow-root --no-browser --NotebookApp.token='robobio'
 
-$CONDA_PREFIX/bin/jupyter lab --ip=0.0.0.0 --port=8888 --allow-root --no-browser --NotebookApp.token='robobio'
+$CONDA_PREFIX/bin/jupyter lab --ip=0.0.0.0 --port=8888 --allow-root --no-browser --NotebookApp.token='robobio' --NotebookApp.allow_origin='*'
